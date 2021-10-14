fa87394401c4fdf2ffc2614d058510a3d072c022
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-10-14 20:22:39 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let User: string = "Gabriel";
+let Pass: string = "Basabe";
+
+let Useringresado: string = dato1.value;
+let Passingresado: string = dato2.value;
+
+if (User == Useringresado && Pass == Passingresado) {
+  console.log("Bienvenido");
+} else {
+  console.log("Fuerra intruso");
+}
