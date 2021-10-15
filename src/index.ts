92c34818b8660b9988f4fb3b2773b8e3688f0d67
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-10-14 21:00:39 -0300

    final esta resuelto

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-let User: string = "Gabriel";
-let Pass: string = "Basabe";
+let user = document.getElementById("Useringresado");
+let pass = document.getElementById("Passingresado");
 
-let Useringresado: string = dato1.value;
-let Passingresado: string = dato2.value;
+Aceptar.addEventListener("click", () => {
+let user: string = "Gabriel";
+let pass: string = "Basabe18..21";
 
-if (User == Useringresado && Pass == Passingresado) {
+let useringresado: string = dato1.value;
+let passingresado: string = dato2.value;
+
+if (user === useringresado && pass === passingresado) {
   console.log("Bienvenido");
 } else {
   console.log("Fuerra intruso");
+}
 }
